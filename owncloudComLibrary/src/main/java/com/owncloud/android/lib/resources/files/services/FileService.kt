5313766730a40a9d42830e34c0f0bf462303f911
agarcia <agarcia@solidgeargroup.com>
2020-06-23 16:10:36 +0200

    Migrate ReadRemoteFolderOperation to kotlin and add it to FileService

--- owncloudComLibrary/src/main/java/com/owncloud/android/lib/resources/files/services/FileService.kt
+++ owncloudComLibrary/src/main/java/com/owncloud/android/lib/resources/files/services/FileService.kt
@@ -25,7 +25,9 @@ package com.owncloud.android.lib.resources.files.services
 
 import com.owncloud.android.lib.common.operations.RemoteOperationResult
 import com.owncloud.android.lib.resources.Service
+import com.owncloud.android.lib.resources.files.RemoteFile
 
 interface FileService : Service {
     fun checkPathExistence(path: String, isUserLogged: Boolean): RemoteOperationResult<Boolean>
+    fun refreshFolder(remotePath: String): RemoteOperationResult<ArrayList<RemoteFile>>
 }
