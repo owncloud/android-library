681e48476e9c39ae9a7926429920d2d1de31a957
agarcia <agarcia@solidgeargroup.com>
2020-07-28 18:08:57 +0200

    Include createFolder as file service operation

--- owncloudComLibrary/src/main/java/com/owncloud/android/lib/resources/files/services/implementation/OCFileService.kt
+++ owncloudComLibrary/src/main/java/com/owncloud/android/lib/resources/files/services/implementation/OCFileService.kt
@@ -22,15 +22,30 @@ package com.owncloud.android.lib.resources.files.services.implementation
 import com.owncloud.android.lib.common.OwnCloudClient
 import com.owncloud.android.lib.common.operations.RemoteOperationResult
 import com.owncloud.android.lib.resources.files.CheckPathExistenceRemoteOperation
+import com.owncloud.android.lib.resources.files.CreateRemoteFolderOperation
 import com.owncloud.android.lib.resources.files.ReadRemoteFolderOperation
 import com.owncloud.android.lib.resources.files.RemoteFile
 import com.owncloud.android.lib.resources.files.services.FileService
 
 class OCFileService(override val client: OwnCloudClient) : FileService {
-    override fun checkPathExistence(path: String, isUserLogged: Boolean): RemoteOperationResult<Boolean> =
+    override fun checkPathExistence(
+        path: String,
+        isUserLogged: Boolean
+    ): RemoteOperationResult<Boolean> =
         CheckPathExistenceRemoteOperation(
             remotePath = path,
             isUserLogged = isUserLogged
+        ).execute(client)
+
+    override fun createFolder(
+        remotePath: String,
+        createFullPath: Boolean,
+        isChunkFolder: Boolean
+    ): RemoteOperationResult<Unit> =
+        CreateRemoteFolderOperation(
+            remotePath = remotePath,
+            createFullPath = createFullPath,
+            isChunksFolder = isChunkFolder
         ).execute(client)
 
     override fun refreshFolder(remotePath: String): RemoteOperationResult<ArrayList<RemoteFile>> =
