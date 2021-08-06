375717f5b1f6ac884d099b527ec60ada60414821
Tanish Mohanty <66123919+tanishpvt@users.noreply.github.com>
2021-08-06 12:22:01 +0530

    Update sample_client/src/main/java/com/owncloud/android/lib/sampleclient/intropage.java
    Co-authored-by: Akshay Vilas Jadhav <52353967+Akshay0701@users.noreply.github.com>

--- sample_client/src/main/java/com/owncloud/android/lib/sampleclient/intropage.java
+++ sample_client/src/main/java/com/owncloud/android/lib/sampleclient/intropage.java
@@ -194,7 +194,7 @@ public class intropage extends AppCompatActivity {
                 next_finish.setText("Next");
                 back.setText("");
             }
-            else if(i==mDots.length-1){
+            else if (i == mDots.length - 1) {
                 next_finish.setEnabled(true);
                 back.setEnabled(true);
                 back.setVisibility(View.VISIBLE);
